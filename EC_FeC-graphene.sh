a6e901724b12e47a13d65c15ddcb9c102063ca4e
lune <lune.sqnm@gmail.com>
2024-02-05 16:04:07 +0900

    slightly modified

--- EC_FeC-graphene.sh
+++ EC_FeC-graphene.sh
@@ -32,3 +32,70 @@ V_delta=0.01
 gpaw -P ${NumCalculatingCore} python EC_SCF_FeC-graphene.py ${SystemName} ${rotX_theta_FeC} ${rotY_theta_FeC} ${rotZ_theta_FeC} ${relativepos} 
 
 python EC_TransmissionCalc.py ${systemname_mod} ${V_low} ${V_high} ${V_delta} 
+
+SystemName=FeC-graphene_ON
+rotX_theta_FeC=0.0
+rotY_theta_FeC=0.0
+rotZ_theta_FeC=0.0
+relativepos=Shift
+
+systemname_mod=${SystemName}_${relativepos}_rotX${rotX_theta_FeC}_rotY${rotY_theta_FeC}_rotZ${rotZ_theta_FeC}
+
+V_low=-0.5
+V_high=0.5
+V_delta=0.01
+
+gpaw -P ${NumCalculatingCore} python EC_SCF_FeC-graphene.py ${SystemName} ${rotX_theta_FeC} ${rotY_theta_FeC} ${rotZ_theta_FeC} ${relativepos} 
+
+python EC_TransmissionCalc.py ${systemname_mod} ${V_low} ${V_high} ${V_delta}
+
+
+SystemName=FeC-graphene_OFF
+rotX_theta_FeC=90.0
+rotY_theta_FeC=0.0
+rotZ_theta_FeC=0.0
+relativepos=Shift
+
+systemname_mod=${SystemName}_${relativepos}_rotX${rotX_theta_FeC}_rotY${rotY_theta_FeC}_rotZ${rotZ_theta_FeC}
+
+V_low=-0.5
+V_high=0.5
+V_delta=0.01
+
+gpaw -P ${NumCalculatingCore} python EC_SCF_FeC-graphene.py ${SystemName} ${rotX_theta_FeC} ${rotY_theta_FeC} ${rotZ_theta_FeC} ${relativepos} 
+
+python EC_TransmissionCalc.py ${systemname_mod} ${V_low} ${V_high} ${V_delta}
+
+
+SystemName=FeC-graphene_ON
+rotX_theta_FeC=0.0
+rotY_theta_FeC=0.0
+rotZ_theta_FeC=0.0
+relativepos=Bridge
+
+systemname_mod=${SystemName}_${relativepos}_rotX${rotX_theta_FeC}_rotY${rotY_theta_FeC}_rotZ${rotZ_theta_FeC}
+
+V_low=-0.5
+V_high=0.5
+V_delta=0.01
+
+gpaw -P ${NumCalculatingCore} python EC_SCF_FeC-graphene.py ${SystemName} ${rotX_theta_FeC} ${rotY_theta_FeC} ${rotZ_theta_FeC} ${relativepos} 
+
+python EC_TransmissionCalc.py ${systemname_mod} ${V_low} ${V_high} ${V_delta}
+
+
+SystemName=FeC-graphene_OFF
+rotX_theta_FeC=90.0
+rotY_theta_FeC=0.0
+rotZ_theta_FeC=0.0
+relativepos=Bridge
+
+systemname_mod=${SystemName}_${relativepos}_rotX${rotX_theta_FeC}_rotY${rotY_theta_FeC}_rotZ${rotZ_theta_FeC}
+
+V_low=-0.5
+V_high=0.5
+V_delta=0.01
+
+gpaw -P ${NumCalculatingCore} python EC_SCF_FeC-graphene.py ${SystemName} ${rotX_theta_FeC} ${rotY_theta_FeC} ${rotZ_theta_FeC} ${relativepos} 
+
+python EC_TransmissionCalc.py ${systemname_mod} ${V_low} ${V_high} ${V_delta} 
