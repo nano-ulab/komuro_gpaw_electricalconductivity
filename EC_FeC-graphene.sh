efc9926e95eba43fe30d72f6d19843c781b2ad07
lune <lune.sqnm@gmail.com>
2024-02-05 14:52:35 +0900

    slightly modified

--- EC_FeC-graphene.sh
+++ EC_FeC-graphene.sh
@@ -3,11 +3,16 @@ SystemName=FeC-graphene_ON
 rotX_theta_FeC=0
 rotY_theta_FeC=0
 rotZ_theta_FeC=0
+relativepos=Overlap
+# relativepos=Shift
+# relativepos=Bridge
+
+systemname_mod=${SystemName}_relativeposition_rotX${rotX_theta_FeC}_$rotY_${rotY_theta_FeC}_rotZ$str${rotZ_theta_FeC}
 
 V_low=-0.5
 V_high=0.5
 V_delta=0.01
 
-gpaw -P 4 python EC_SCF_FeC-graphene.py ${SystemName} ${rotX_theta_FeC} ${rotY_theta_FeC} ${rotZ_theta_FeC}
-
-python EC_TransmissionCalc.py ${SystemName} ${V_low} ${V_high} ${V_delta} 
+gpaw -P 4 python EC_SCF_FeC-graphene.py ${SystemName}_ ${rotX_theta_FeC} ${rotY_theta_FeC} ${rotZ_theta_FeC} ${relativepos} 
+${relativepos}
+python EC_TransmissionCalc.py ${systemname_mod} ${V_low} ${V_high} ${V_delta} 
